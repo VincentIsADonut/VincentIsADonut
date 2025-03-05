64e34b0dbd45f725094208b36b8e5946577882cd
Vincent <1livin2@hdsb.ca>
2025-03-05 08:32:53 -0500

    INSULT-INATOR

---
+++ INSULT-INATOR.py
@@ -0,0 +1,9 @@
+print("Say hello to my insult-inator!")
+print("What is your name?") 
+name = input()
+print ("What is greater than 1?") 
+number = int(input())
+if number > 1:
+    print("Good job! ", name, ", you are are NOT a donut!", sep="") 
+else: 
+    print("Oh my days! ", name, ", you are are a donut!", sep="") 
